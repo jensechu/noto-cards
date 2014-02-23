07bb59cf2257d0c28385a32e554dea81f6345da9
Jensen <jensechu@gmail.com>
2014-02-22 21:24:40 -0800

    :blush: Randomly displays cards, toggles english

--- js/main.js
+++ js/main.js
@@ -1,22 +1,46 @@
 $(document).ready(function(){
+
     function loadCards() {
-        $.getJSON('/js/cards.js', function(data) {
+        $.getJSON('/js/cards.json', function(data) {
             var items = [];
             $.each( data.cards, function( i, val ) {
-                console.log(i);
                 items.push( 
-                    "<div class='card' + <h1 class='japanese' data-pronounciation='" + data.cards[i].pronounciation + "'>" + data.cards[i].japanese + "</h1> <h1 class='english'>" + data.cards[i].english + "</h1></div>" );
+                    "<div class='card is-hidden'><h1>" + data.cards[i].japanese + "</h1> <h1 class='english is-hidden'>" + data.cards[i].english + "</h1></div>" );
             });
-            
             templateItems = items.join( "" )
-
             $(templateItems).appendTo( "#container" );
             
         })
-            .error(function(j, t, e) { console.log(e);})
+	.done(function() { 
+	    setInterval( function(){ displayRandomCard(); }, 10000 );
+	    showTranslation();
+	    newCard();
+	})
         return false;
     }
 
+    function showTranslation() {
+	$('.card').on('click', function(){
+	    $(this).find('.english').toggleClass('is-hidden');
+	});
+    }
+
+    function displayRandomCard(){
+	cards = $('.card');
+	english = $('.card .english');
+	displayCard = Math.floor( Math.random() * cards.length )
+	
+	$(cards).addClass('is-hidden');
+	$(english).addClass('is-hidden');
+	$(cards[displayCard]).removeClass('is-hidden');
+    }
+
+    function newCard(){
+	$('.toggle').on('click', function(){
+	    displayRandomCard();
+	});
+    }
+
     loadCards();
-    console.log(window.cards);
+
 });
