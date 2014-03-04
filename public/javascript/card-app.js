ec2e6a64e3aef1b111c88f53c035087ec7e6c320
Jensen <jensechu@gmail.com>
2014-03-04 08:46:16 -0800

    :candy: Making my own class for hiding/showing

--- public/javascript/card-app.js
+++ public/javascript/card-app.js
@@ -1,7 +1,31 @@
-/** @jsx React.DOM */
 console.log('hi');
 
-React.renderComponent(
-  React.DOM.h1(null, 'Hello, world!'),
-  document.getElementById('example')
-);
+function Card (front, back, deck) {
+    this.front = front;
+    this.back = back;
+    this.deck = deck;
+}
+
+$(document).ready(function(){
+    function Card ($elm) {
+	this.$elm = $elm;
+	this.$front = $elm.find('front');
+	this.$back = $elm.find('back');
+	this.deck = $elm.data('deck');
+
+	this.flipCard = function($elm, $front, $back){
+	    $elm.on('click', function(){
+		// These log but aren't doing anything?
+		$front.addClass('is-hidden');
+		$back.toggleClass('is-hidden');
+	    });
+	}
+
+	this.flipCard(this.$elm, this.$front, this.$back);
+    }
+
+    $('.card').each(function(){
+	$elm = $(this);
+	new Card($elm);
+    });
+});
